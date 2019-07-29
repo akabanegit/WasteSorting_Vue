4678445146fe1954717f01090b1c374eef7e7264
akabane_bijiben <631544250@qq.com>
2019-07-29 19:32:24 +0800

    删去scoll

--- src/views/waste/gan.vue
+++ src/views/waste/gan.vue
@@ -1,21 +1,32 @@
 <template>
     <div class="gan-container">
-        <div class="mui-scroll-wrapper">
+        <!-- <div class="mui-scroll-wrapper">
 	        <div class="mui-scroll">
-                div
+
 	        </div>
-        </div>
+        </div> -->
+
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
     </div>
 </template>
 
 <script>
-import mui from '../../css/hello-mui/js/mui.js'
-
+// import mui from '../../css/hello-mui/js/mui.js'
+// mui('.mui-scroll-wrapper').scroll({
+//             deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+//         });
 export default {
     mounted(){
-        mui('.mui-scroll-wrapper').scroll({
-            deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-        });
+        
     }
 }
 </script>
